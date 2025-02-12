eaa185906b1202ee3cd33047c0dd002b610614e7
Arturo Mejia <armejia02@gmail.com>
2025-02-12 12:34:58 -0600

    bug fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,44 +13,40 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <Head>
-          <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>Vintage Outdoors</title>
-          <meta name="description" content="Short app description" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Vintage Outdoors</title>
+        <meta name="description" content="Short app description" />
 
-          <meta
-            name="keywords"
-            content="Outdoors, Vintage, Pool, Vintage Outdoors, "
-          />
-          <meta name="author" content="Vintage Outdoors" />
-          <meta name="robots" content="index" />
+        <meta
+          name="keywords"
+          content="Outdoors, Vintage, Pool, Vintage Outdoors, "
+        />
+        <meta name="author" content="Vintage Outdoors" />
+        <meta name="robots" content="index" />
 
-          <meta property="og:title" content="Vintage Outdoors" />
-          <meta
-            property="og:description"
-            content="Vintage Outdoors landing page"
-          />
-          <meta
-            property="og:image"
-            content="https://prueba-tecnica-isning.vercel.app/logo.png"
-          />
-          <meta
-            property="og:url"
-            content="https://prueba-tecnica-isning.vercel.app/"
-          />
-          <meta property="og:type" content="website" />
+        <meta property="og:title" content="Vintage Outdoors" />
+        <meta
+          property="og:description"
+          content="Vintage Outdoors landing page"
+        />
+        <meta
+          property="og:image"
+          content="https://prueba-tecnica-isning.vercel.app/logo.png"
+        />
+        <meta
+          property="og:url"
+          content="https://prueba-tecnica-isning.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
 
-          <meta name="theme-color" content="#ffffff" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta
-            name="apple-mobile-web-app-status-bar-style"
-            content="default"
-          />
-          <meta name="apple-mobile-web-app-title" content="Vintage Outdoors" />
-        </Head>
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Vintage Outdoors" />
       </Head>
+
       <main className={`${montserrat.variable} font-montserrat`}>
         <Component {...pageProps} />
       </main>
